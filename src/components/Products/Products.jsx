26ca374471e4872fdfd2d1cb764fda72714b2786
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-20 20:33:51 +0100

    products can be added to the cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,33 +5,6 @@ import Product from './Product/Product'
 
 import useStyles from './ProductsStyles'
 
-const products = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'running shoes',
-    price: '$5',
-    image:
-      'https://www.runtastic.com/blog/wp-content/uploads/2021/06/thumbnail_1200x800.jpg',
-  },
-  {
-    id: 2,
-    name: 'MacBook',
-    description: 'Apple Macbook',
-    price: '$15',
-    image:
-      'https://www.apple.com/v/macbook-pro-14-and-16/a/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png',
-  },
-  {
-    id: 3,
-    name: 'Pen',
-    description: 'Morning Glory Pen',
-    price: '$3',
-    image:
-      'https://file.mk.co.kr/meet/neds/2020/07/image_readtop_2020_753575_15954697494290264.jpg',
-  },
-]
-
 const Products = ({ products, onAddToCart }) => {
   const classes = useStyles()
   return (
