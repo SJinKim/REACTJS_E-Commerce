9bafdca2d8db939c3f21b8a5ce79c0bfdcaf2773
SeungJin Kim <ksj_1207@hotmail.com>
2022-01-20 21:28:28 +0100

    Cart remove add change quantity works

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Grid, Typography, Button, Container } from '@material-ui/core'
 import useStyles from './CartStyles'
 import CardItem from './CartItem/CartItem'
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles()
 
   const EmptyCart = () => (
     <Typography variant='subtitle1'>
-      You have no items in your shopping cart! Start shopping!
+      You have no items in your shopping cart
+      <br />
+      <Link to='/' className={classes.link}>
+        click here to shop!
+      </Link>
     </Typography>
   )
 
@@ -17,7 +27,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CardItem item={item} />
+            <CardItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -32,6 +46,7 @@ const Cart = ({ cart }) => {
             type='button'
             variant='contained'
             color='secondary'
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
